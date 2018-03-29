812d2777a44f956d16a225f8000a20903769455f
unknown <mmreyes13@gmail.com>
2018-03-28 20:34:05 -0400

    added eventListener to ENTER key and check mark button

--- app.js
+++ app.js
@@ -1,16 +1,8 @@
 // Budget Controller
 var budgetController = (function() {
-    var x = 23;
 
-    var add = function(a) {
-        return x + a;
-    }
+    // Some code
 
-    return {
-        publicTest: function(b) {
-            return add(b);
-        }
-    }
 })();
 
 
@@ -18,7 +10,9 @@ var budgetController = (function() {
 
 var UIController = (function() {
 
+
     // Some code
+
 })();
 
 
@@ -26,11 +20,27 @@ var UIController = (function() {
 
 var controller = (function(budgetCtrl, UICtrl) {
 
-    var z = budgetCtrl.publicTest(5);
-    return {
-        anotherPublic: function() {
-            console.log(z);
-        }
+    var ctrlAddItem = function() {
+
+        // 1. Get the field input data
+        // 2. Add the item to the budget controller
+        // 3. Add the item to the UI
+        // 4. Calculate the budget
+        // 5. Display the budget
+
+        console.log("Test");
     }
+
+    document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
+
+    document.addEventListener("keypress", function(event) {
+
+        if (event.keyCode === 13) {
+            ctrlAddItem();
+
+
+        }
+
+    });
 
 })(budgetController, UIController);
