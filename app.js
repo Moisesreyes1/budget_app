f92253a465180b9ebbcc98fcca325673dfe00a2b
unknown <mmreyes13@gmail.com>
2018-04-03 21:20:00 -0400

    adding input values to UI Controller

--- app.js
+++ app.js
@@ -7,11 +7,31 @@ var budgetController = (function() {
 
 
 
-
+// UI Controller
 var UIController = (function() {
 
+    var DOMstrings = {
+        inputType: ".add__type",
+        inputDescription: ".add__description",
+        inputValue: "add_value",
+        inputBtn: ".add__btn"
+    };
 
-    // Some code
+    return {
+        getInput: function() {
+
+
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+
+        getDOMstrings: function() {
+            return DOMstrings;
+        }
+    };
 
 })();
 
@@ -20,9 +40,13 @@ var UIController = (function() {
 
 var controller = (function(budgetCtrl, UICtrl) {
 
+    var DOM = UICtrl.getDOMstrings();
+
     var ctrlAddItem = function() {
 
         // 1. Get the field input data
+        var input = UICtrl.getInput();
+        console.log(input);
         // 2. Add the item to the budget controller
         // 3. Add the item to the UI
         // 4. Calculate the budget
@@ -31,14 +55,12 @@ var controller = (function(budgetCtrl, UICtrl) {
         console.log("Test");
     }
 
-    document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
 
     document.addEventListener("keypress", function(event) {
 
         if (event.keyCode === 13) {
             ctrlAddItem();
-
-
         }
 
     });
