27c50c2d651387587d585d34a5c3e1aad0c62690
unknown <mmreyes13@gmail.com>
2018-04-10 17:41:22 -0400

    created init function

--- app.js
+++ app.js
@@ -40,31 +40,40 @@ var UIController = (function() {
 
 var controller = (function(budgetCtrl, UICtrl) {
 
-    var DOM = UICtrl.getDOMstrings();
+    var setupEventListeners = function() {
+        var DOM = UICtrl.getDOMstrings();
+
+        document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
+
+        document.addEventListener("keypress", function(event) {
+            if (event.keyCode === 13) {
+                ctrlAddItem();
+            }
+        });
+    };
+
 
     var ctrlAddItem = function() {
 
         // 1. Get the field input data
         var input = UICtrl.getInput();
-        console.log(input);
+
         // 2. Add the item to the budget controller
         // 3. Add the item to the UI
         // 4. Calculate the budget
         // 5. Display the budget
 
-        console.log("Test");
-    }
+    };
 
-    document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
-
-    document.addEventListener("keypress", function(event) {
-
-        if (event.keyCode === 13) {
-            ctrlAddItem();
+    return {
+        init: function() {
+            console.log("Application has started.");
+            setupEventListeners();
         }
-
-    });
+    };
 
 })(budgetController, UIController);
+
+controller.init();
 
 //Test
